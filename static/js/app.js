e81050da1e706d37ea44049ced1341d6594daf22
ctal <c_tallent@sbcglobal.net>
2022-02-02 16:03:31 -0600

    Updated app.js.

--- static/js/app.js
+++ static/js/app.js
@@ -6,7 +6,6 @@ var tbody = d3.select("tbody");  // Declare var
 // create table, first, clear out any existing data
 function buildTable(data) {
     tbody.html("");
-}
 
 // Create forEach function to loop through array and add rows of data to table
     data.forEach((dataRow) => {
@@ -16,5 +15,26 @@ function buildTable(data) {
         let cell = row.append("td");
         cell.text(val);
         }
-    );    
-}); // Loop thru each obj in array, append row to HTML table, Add value from obj into cell
+      );    
+    }); // Loop thru each obj in array, append row to HTML table, Add value from obj into cell
+}
+
+// Create function to select first element matching selector string - #dateime, grab it and hold it
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    //Apply filter to match datetime value to filtered date value
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild table using filtered data
+    // Note:  if no date was entered, filteredData will just be orig. tableData
+        buildTable(filteredData);
+};
+// Link code to filter button, tell D3 to execute handleClick function when filter-btn clicked
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build table when page loads
+buildTable(tableData);
